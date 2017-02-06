7d0f3be5e1578b5337b4edfd942eb54a2b636a3a
Will Wright <will@clazzoo.com>
2017-02-06 12:21:21 +0000

    Initial Commit

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// After Meteor loads in the browser, render my app to the DOM:
+Meteor.startup(() => {
+  // React render call:
+
+});
