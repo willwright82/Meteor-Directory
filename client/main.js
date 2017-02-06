4e7a0a675a9f2ae9e93cde4633f67e47e0d5662d
Will Wright <will@clazzoo.com>
2017-02-06 12:25:38 +0000

    Hello World Boilerplate build

--- client/main.js
+++ client/main.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = () => {
+  return (
+    <div>Hello There!</div>
+  );
+};
+
 // After Meteor loads in the browser, render my app to the DOM:
 Meteor.startup(() => {
   // React render call:
-
+  ReactDOM.render(<App />, document.querySelector('.container'));
 });
