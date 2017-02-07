c20517fede65d85c6fbf79c9b52d4077c5254d4e
Will Wright <will@clazzoo.com>
2017-02-07 10:42:44 +0000

    Refactoring EmployeeList from funcional component to class

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
-const EmployeeList = (props) => {
-  // props.employees => an array of employee objects
+const PER_PAGE = 20;
 
-  return(
-  <div>
-    <div className="employee-list">
-      {props.employees.map(employee =>
-        <EmployeeDetail key={employee._id} employee={employee} />
-      )}
-    </div>
-  </div>
-  );
+class EmployeeList extends Component {
+  render() {
+    // props.employees => an array of employee objects
+    return(
+      <div>
+        <div className="employee-list">
+          {this.props.employees.map(employee =>
+            <EmployeeDetail key={employee._id} employee={employee} />
+          )}
+        </div>
+        <button onClick={() => Meteor.subscribe('employees', 40) }
+          className="btn-lg btn-primary">
+          Load more...
+        </button>
+      </div>
+    );
+  }
 };
 
 export default createContainer(() => {
   // set up subscription
-  Meteor.subscribe('employees');
+  Meteor.subscribe('employees', PER_PAGE);
 
   // return an object. Whatever we rturn will be sent to EmployeeList as props
   return { employees: Employees.find({}).fetch() };
--- server/main.js
+++ server/main.js
@@ -31,7 +31,7 @@ Meteor.startup(() => {
     });
   }
 
-  Meteor.publish('employees', function() {
-    return Employees.find({}, { limit: 20 });
+  Meteor.publish('employees', function(per_page) {
+    return Employees.find({}, { limit: per_page });
   });
 });
