67100e3918d6bd909b2ed4120115b3079889721b
Will Wright <will@clazzoo.com>
2017-02-07 10:15:51 +0000

    Display employee details inside employee list

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
+import EmployeeDetail from './employee_detail';
 
-const EmployeeList = () => {
+const EmployeeList = (props) => {
+  // props.employees => an array of employee objects
+
   return(
   <div>
     <div className="employee-list">
-      Employee List
+      {props.employees.map(employee => <EmployeeDetail employee={employee} />)}
     </div>
   </div>
   );
